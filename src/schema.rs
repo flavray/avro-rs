c0dca2f79cb5cff125bdea6d7ab5dc3bf8d6a2aa
Jason Boatman <jason.boatman@multiscalehn.com>
2018-07-16 15:49:08 -0400

    Implemented parsing canonical form of schemas.

--- src/schema.rs
+++ src/schema.rs
@@ -149,7 +149,8 @@ impl Name {
         if self.name.contains('.') {
             self.name.clone()
         } else {
-            let namespace = self.namespace
+            let namespace = self
+                .namespace
                 .as_ref()
                 .map(|s| s.as_ref())
                 .or(default_namespace);
@@ -244,6 +245,15 @@ impl Schema {
             Value::Array(ref data) => Schema::parse_union(data),
             _ => Err(ParseSchemaError::new("Must be a JSON string, object or array").into()),
         }
+    }
+
+    /// Converts `self` into its [Parsing Canonical Form].
+    ///
+    /// [Parsing Canonical Form]:
+    /// https://avro.apache.org/docs/1.8.2/spec.html#Parsing+Canonical+Form+for+Schemas
+    pub fn canonical_form(&self) -> String {
+        let json = serde_json::to_value(self).unwrap();
+        parsing_canonical_form(&json)
     }
 
     /// Parse a `serde_json::Value` representing a primitive Avro type into a
@@ -426,19 +436,19 @@ impl Serialize for Schema {
                 map.serialize_entry("type", "array")?;
                 map.serialize_entry("items", &*inner.clone())?;
                 map.end()
-            },
+            }
             Schema::Map(ref inner) => {
                 let mut map = serializer.serialize_map(Some(2))?;
                 map.serialize_entry("type", "map")?;
                 map.serialize_entry("values", &*inner.clone())?;
                 map.end()
-            },
+            }
             Schema::Union(ref inner) => {
                 let mut seq = serializer.serialize_seq(Some(2))?;
                 seq.serialize_element("null")?;
                 seq.serialize_element(&*inner.clone())?;
                 seq.end()
-            },
+            }
             Schema::Record {
                 ref name,
                 ref doc,
@@ -459,7 +469,7 @@ impl Serialize for Schema {
                 }
                 map.serialize_entry("fields", fields)?;
                 map.end()
-            },
+            }
             Schema::Enum {
                 ref name,
                 ref symbols,
@@ -470,14 +480,14 @@ impl Serialize for Schema {
                 map.serialize_entry("name", &name.name)?;
                 map.serialize_entry("symbols", symbols)?;
                 map.end()
-            },
+            }
             Schema::Fixed { ref name, ref size } => {
                 let mut map = serializer.serialize_map(None)?;
                 map.serialize_entry("type", "fixed")?;
                 map.serialize_entry("name", &name.name)?;
                 map.serialize_entry("size", size)?;
                 map.end()
-            },
+            }
         }
     }
 }
@@ -497,6 +507,112 @@ impl Serialize for RecordField {
 
         map.end()
     }
+}
+
+/// Parses a **valid** avro schema into the Parsing Canonical Form.
+/// https://avro.apache.org/docs/1.8.2/spec.html#Parsing+Canonical+Form+for+Schemas
+fn parsing_canonical_form(schema: &serde_json::Value) -> String {
+    match schema {
+        serde_json::Value::Number(n) => pcf_float(n),
+        serde_json::Value::Object(map) => pcf_map(map),
+        serde_json::Value::String(s) => pcf_string(s),
+        serde_json::Value::Array(v) => pcf_array(v),
+        _ => unreachable!(),
+    }
+}
+
+fn pcf_map(schema: &Map<String, serde_json::Value>) -> String {
+    use std::borrow::Cow;
+    // Look for the namespace variant up front.
+    let ns = schema.get("namespace").and_then(|v| v.as_str());
+    let mut fields = Vec::new();
+    for (k, v) in schema {
+        // Reduce primitive types to their simple form. ([PRIMITIVE] rule)
+        if schema.len() == 1 && k == "type" {
+            // Invariant: function is only callable from a valid schema, so this is acceptable.
+            if let serde_json::Value::String(s) = v {
+                return pcf_string(s);
+            }
+        }
+
+        // Strip out unused fields ([STRIP] rule)
+        if field_ordering_position(k).is_none() {
+            continue;
+        }
+
+        // Fully qualify the name, if it isn't already ([FULLNAMES] rule).
+        if k == "name" {
+            // Invariant: Only valid schemas. Must be a string.
+            let name = v.as_str().unwrap();
+            let n = match ns {
+                Some(namespace) if !name.contains('.') => {
+                    Cow::Owned(format!("{}.{}", namespace, name))
+                }
+                _ => Cow::Borrowed(name),
+            };
+
+            fields.push((k, format!("{}:{}", pcf_string(k), pcf_string(&*n))));
+            continue;
+        }
+
+        // Strip off quotes surrounding "size" type, if they exist ([INTEGERS] rule).
+        if k == "size" {
+            let i = match v.as_str() {
+                Some(s) => s.parse::<i64>().expect("Only valid schemas are accepted!"),
+                None => v.as_i64().unwrap(),
+            };
+            fields.push((k, format!("{}:{}", pcf_string(k), i)));
+            continue;
+        }
+
+        // For anything else, recursively process the result.
+        fields.push((
+            k,
+            format!("{}:{}", pcf_string(k), parsing_canonical_form(v)),
+        ));
+    }
+
+    // Sort the fields by their canonical ordering ([ORDER] rule).
+    fields.sort_unstable_by_key(|(k, _)| field_ordering_position(k).unwrap());
+    let inter = fields
+        .into_iter()
+        .map(|(_, v)| v)
+        .collect::<Vec<_>>()
+        .join(",");
+    format!("{{{}}}", inter)
+}
+
+fn pcf_array(arr: &[serde_json::Value]) -> String {
+    let inter = arr
+        .iter()
+        .map(parsing_canonical_form)
+        .collect::<Vec<String>>()
+        .join(",");
+    format!("[{}]", inter)
+}
+
+fn pcf_string(s: &str) -> String {
+    format!("\"{}\"", s)
+}
+
+fn pcf_float(schema: &serde_json::Number) -> String {
+    schema.as_i64().unwrap().to_string()
+}
+
+// Used to define the ordering and inclusion of fields.
+fn field_ordering_position(field: &str) -> Option<usize> {
+    let v = match field {
+        "name" => 1,
+        "type" => 2,
+        "fields" => 3,
+        "symbols" => 4,
+        "items" => 5,
+        "values" => 6,
+        "size" => 7,
+        _ => return None,
+    };
+
+    Some(v)
 }
 
 #[cfg(test)]
