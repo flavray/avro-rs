082f80ffd4e072d387ce14df1a9d5b437e534126
Michael Krolikowski <mkroli@yahoo.de>
2019-12-22 17:45:30 +0100

    tests for array/map decoding

--- src/decode.rs
+++ src/decode.rs
@@ -162,3 +162,41 @@ pub fn decode<R: Read>(schema: &Schema, reader: &mut R) -> Result<Value, Error> 
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::Value::{Bytes, Int, Array, Map};
+
+    #[test]
+    fn test_decode_array_without_size() {
+        let mut input: &[u8] = &[6, 2, 4, 6, 0];
+        let result = decode(&Schema::Array(Box::new(Schema::Int)), &mut input);
+        assert_eq!(Array(vec!(Int(1), Int(2), Int(3))), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_array_with_size() {
+        let mut input: &[u8] = &[5, 6, 2, 4, 6, 0];
+        let result = decode(&Schema::Array(Box::new(Schema::Int)), &mut input);
+        assert_eq!(Array(vec!(Int(1), Int(2), Int(3))), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_map_without_size() {
+        let mut input: &[u8] = &[0x02, 0x08, 0x74, 0x65, 0x73, 0x74, 0x02, 0x00];
+        let result = decode(&Schema::Map(Box::new(Schema::Int)), &mut input);
+        let mut expected = HashMap::new();
+        expected.insert(String::from("test"), Int(1));
+        assert_eq!(Map(expected), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_map_with_size() {
+        let mut input: &[u8] = &[0x01, 0x0C, 0x08, 0x74, 0x65, 0x73, 0x74, 0x02, 0x00];
+        let result = decode(&Schema::Map(Box::new(Schema::Int)), &mut input);
+        let mut expected = HashMap::new();
+        expected.insert(String::from("test"), Int(1));
+        assert_eq!(Map(expected), result.unwrap());
+    }
+}
